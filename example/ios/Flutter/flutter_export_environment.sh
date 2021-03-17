e528ecd55c130029401d65d76c9bd23b04f7c4e3
Francisco Jordano <fjordano@mozilla.com>
2021-03-17 22:48:01 +0000

    Updating library to use intl 1.0.17

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/arcturus/dev/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/arcturus/dev/git/libertonia/flutter_date_pickers/example"
+export "FLUTTER_TARGET=/Users/arcturus/dev/git/libertonia/flutter_date_pickers/example/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/arcturus/dev/git/libertonia/flutter_date_pickers/example/.dart_tool/package_config.json"
